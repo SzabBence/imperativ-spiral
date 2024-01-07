c5878283f8eb9e54a4ab6b8eb4ce772c4c1cda29
SzabBence <91546960+SzabBence@users.noreply.github.com>
2024-01-07 10:32:29 +0100

    add new mapping struct

--- matrix.h
+++ matrix.h
@@ -10,6 +10,18 @@ typedef struct
     int loadingFailed;  
 } Matrix;
 
+typedef struct 
+{
+    int x1;
+    int x2;
+    int x3;
+    int x4;
+    int y1;
+    int y2;
+    int y3;
+    int y4;
+} RotationMapping;
+
 int countLines(char *filePath);
 int countColumns(char *filePath);
 int file_exists(char *filename);
@@ -23,5 +35,6 @@ void DisplayMatrix(Matrix *matrix);
 void WriteMatrixToFileFromTerminal(Matrix *matrix);
 void FillMatrixWithDefaultValues(Matrix *matrix);
 Matrix LoadMatrixFromTerminal();
-
+void GenerateMatrix(Matrix *matrix);
+int main();
 #endif
